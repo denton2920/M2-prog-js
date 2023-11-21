d52ffb67d94f08533af954e572acc2a874dd0332
Dennis <37660@ma-web.nl>
2023-11-21 12:27:43 +0100

    opdracht 2

--- 01a/app.js
+++ 01a/app.js
@@ -2,7 +2,14 @@ class App
 {
     runApplication()
     {
-        console.log("hello world!");
+        let appNaam = ("hallo");
+        let versienummer = 12;
+        let versiedatum = ("21-11-2023");
+        let authert = ("Dennis Ton");
+        let coppyright = ("Dennis Ton");
+        let distributeur = ("Dennis Ton");
+        let darkmode = false;
+        console.log("appNaam: "+ appNaam + "\nversienummer: "+ versienummer + "\nversiedatum: "+ versiedatum + "\nauthert: "+ authert + "\ncoppyright: " + coppyright + "\ndistributeur: " + distributeur + "\nhdarkmode: "+ darkmode);
     }
 }
 
