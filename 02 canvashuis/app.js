18d031e01349c0eeac94a4b166165f0fda947638
Dennis <37660@ma-web.nl>
2023-11-23 12:10:18 +0100

    opdracht 2.2

--- 02 canvashuis/app.js
+++ 02 canvashuis/app.js
@@ -5,6 +5,9 @@ class App
         console.log("hello world!");
         let canvas = document.getElementById("canvasId");
         let g = canvas.getContext("2d");
+        g.fillStyle = "blue";
+        g.fillRect(0,0,canvas.width,canvas.height);
+        g.fillStyle = "green"
         g.fillRect(0,0,10,10);
         console.log(canvas);
     }
