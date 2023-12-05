90a6ce3d77b7121c253d8b639ce5a044a00f887b
Dennis <37660@ma-web.nl>
2023-12-05 11:57:36 +0100

    opdracht 4.2

--- 04 functions/app.js
+++ 04 functions/app.js
@@ -1,7 +1,17 @@
+function argumentsAreHandy(shoutout)
+{
+    console.log("do you want to give a shoutout?");
+    console.log(shoutout);
+}
+
+argumentsAreHandy("Super Shout out");
+argumentsAreHandy("hou het netjes!");
+argumentsAreHandy("dag allemaal");
+
 function superMooieGlobalFuntion()
 {
     console.log("ik ben global");
-    console.log("dus je mag mij overal aanroepen")
+    console.log("dus je mag mij overal aanroepen");
 }
 superMooieGlobalFuntion();
 superMooieGlobalFuntion();
