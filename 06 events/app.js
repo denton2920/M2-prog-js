d40ca24d78d087aa3e464d1cd017fe7d5ae254b5
Dennis <37660@ma-web.nl>
2024-01-16 10:38:50 +0100

    opdracht 6

--- 06 events/app.js
+++ 06 events/app.js
@@ -1,6 +1,13 @@
 /*   {
-        const b = document.getElementById("myButton")
-        b.addEventListener("click",(evt)=>{})
+     const b = document.getElementById("button2")
+        uiButten.addEventListener("click",(evt)=>
+        {
+            const para = document.createElement("h1");
+            const node = document.createTextNode("natuur ");
+            para.appendChild(node);
+            document.body.appendChild(para);
+        });
+        uiButten.addEventListener("click",localeFuntion)
     }*/
 
 class App
@@ -9,12 +16,51 @@ class App
     {
         console.log("hello world");
         const uiButten = document.getElementById("myButton");
-        uiButten.addEventListener("click",(evt)=>{});
-        let localeFuntion = function (e)
+        uiButten.addEventListener("click",(evt)=>
         {
-            console.log("click!")
-        }
-        uiButten,addEventListener("click",localeFuntion)
+            const para = document.createElement("p");
+            const node = document.createTextNode("this is new. ");
+            para.appendChild(node);
+            document.body.appendChild(para);
+        });
+        
+
+        const b = document.getElementById("button2")
+        b.addEventListener("click",(evt)=>
+        {
+            const para = document.createElement("h1");
+            const node = document.createTextNode("natuur ");
+            para.appendChild(node);
+            document.body.appendChild(para);
+        });
+
+        const c = document.getElementById("button2")
+        c.addEventListener("click",(evt)=>
+        {
+            const para = document.createElement("p");
+            const node = document.createTextNode("wat een mooie natuur ");
+            para.appendChild(node);
+            document.body.appendChild(para);
+        });
+
+        const d = document.getElementById("button2")
+        d.addEventListener("click",(evt)=>
+        {
+            const img = new Image()
+            img.src = "img/plaat.jpg"
+            document.body.appendChild(img)         
+        });
+
+        const divAanpassen = document.getElementById("div")
+        divAanpassen.addEventListener("click",(evt)=>{
+            divAanpassen.style.backgroundColor = "green"
+            const para = document.createElement("h1");
+            const node = document.createTextNode("clicked a div ");
+            para.appendChild(node);
+            document.body.appendChild(para);
+
+        })
+        
     }
 }
 
